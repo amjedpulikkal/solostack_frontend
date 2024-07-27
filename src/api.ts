320e5f971b2ee8e15ef8bf2b8859907f7d5f0e45
amjedpulikkal <amjed0pulikkal@gmail.com>
2024-07-27 22:52:01 +0530

    fexid

--- src/api.ts
+++ src/api.ts
@@ -38,7 +38,7 @@
     
 // }
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api/v1";
 
 const createEndpoint = (path) => `${apiBaseUrl}${path}`;
 
